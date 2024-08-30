e6e78c5cfdc435974268ec554e6074096804feb1
Memoyu <mmy6076@outlook.com>
2024-08-30 20:34:19 +0800

    ⚡️ 增加顶部导航栏

---
+++ src/Memo.Blog.App/Components/Layout/Navigations/TopNavigation.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Memo.Blog.App.Components.Layout.Navigations;
+
+public partial class TopNavigation
+{
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs>? OnClick { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Inject]
+    protected IJSRuntime JS { get; set; } = default!;
+
+    private async Task HandleNavigateToBackClick(MouseEventArgs e)
+    {
+        if (OnClick == null)
+        {
+            await JS.InvokeVoidAsync("history.back");
+            return;
+        }
+
+        await OnClick.Value.InvokeAsync(e);
+    }
+}
--- src/Memo.Blog.App/Components/Pages/Article/ArticleEditPage.razor.cs
+++ src/Memo.Blog.App/Components/Pages/Article/ArticleEditPage.razor.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.JSInterop;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace Memo.Blog.App.Components.Pages.Article
 {
     public partial class ArticleEditPage
     {
+        [Inject]
+        protected IJSRuntime JS { get; set; } = default!;
 
+
+        private async Task NavigateToBack()
+        {
+            await JS.InvokeVoidAsync("history.back");
+        }
     }
 }
